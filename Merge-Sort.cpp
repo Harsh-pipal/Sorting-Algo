e11c6596c5fe6e8857963e95929930919f2c3f7a
Harsh PIpal <73478379+Harsh-pipal@users.noreply.github.com>
2022-03-14 19:02:15 +0530

    Create Merge-Sort.cpp

---
+++ Merge-Sort.cpp
@@ -0,0 +1,165 @@
+//Author @Harsh_xerus   
+
+#include<bits/stdc++.h>
+
+//#include <ext/pb_ds/assoc_container.hpp>
+//#include <ext/pb_ds/tree_policy.hpp>
+//#define rep(i,n) for (int i = 0; i < n; i++)
+#define mem0(arr) memset(arr, 0, sizeof(arr))
+#define mod 1000000007
+#define mod2 998244353
+#define pb push_back
+#define ff first
+#define ss second
+#define ll long long 
+#define pll pair<ll,ll>
+#define pss pair<ll,string>
+#define vpss vector<pss>
+#define vll vector<ll>
+#define vpll vector<pll>
+#define INF 1e18
+#define nl '\n'
+#define vi vector<int>
+#define rep(i, a, b) for (int i = a; i < b; i++)
+const ll maxn = 1e6 + 2;
+const double PI = 3.141592653589793238460;
+typedef std::complex<double> Complex;
+typedef std::valarray<Complex> CArray;
+typedef unsigned long long ull;
+typedef long double lld;
+using namespace std;
+// Pragmas
+//#pragma GCC optimize("O3,unroll-loops")
+//#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+
+//------------------------------Debugger-----------------------------------//
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+//--------------------------Helper_functions-------------------------------//
+
+//-------------------------------GCD---------------------------------------//
+
+ll gcd(ll a, ll b){if (b == 0)return a;return gcd(b, a % b);}
+
+//-------------------------------LCM---------------------------------------//
+
+ll lcm(ll a, ll b){return (a * b) / gcd(a, b);}
+
+//----------------------------Factorial------------------------------------//
+
+ll fact(ll x){if(x==0)return 1;return (x*fact(x-1))%mod;}
+
+//-----------------------------Bin_Expo------------------------------------//
+
+ll binpow(ll a,ll b) {
+    ll res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = (res * a);
+        a = (a * a);
+        b >>= 1;
+    }
+    return res;
+}
+
+//-------------------------Sum/Count of digits----------------------------//
+
+ll sd(ll x){ll temp=0;while(x>0){temp+=x%10;x/=10;}return temp;}
+ll cd(ll x){ll temp=0;while(x>0){temp++;x/=10;}return temp;}
+
+//-------------------------------START------------------------------------//
+
+void merge(ll arr[], ll left ,ll mid ,ll right){
+    ll leftsize=mid-left+1;
+    ll rightsize=right-mid;
+    ll la[leftsize+1],ra[rightsize+1];
+
+    for(int i=0;i<leftsize;i++){
+        la[i]=arr[i+left];
+    }
+    for(int i=0;i<rightsize;i++){
+        ra[i]=arr[i+mid+1];
+    }
+    la[leftsize]=INF;
+    ra[rightsize]=INF;
+
+    ll pt1=0,pt2=0;
+    for(int i=left;i<=right;i++){
+        if(la[pt1]<=ra[pt2]){
+            arr[i]=la[pt1];
+            pt1++;
+        }
+        else{
+            arr[i]=ra[pt2];
+            pt2++;
+        }
+    }
+}
+
+void mergesort(ll arr[],ll l, ll r){
+    if(l==r)
+        return;
+    ll mid=(l+r)/2;
+    mergesort(arr,l,mid);
+    mergesort(arr,mid+1,r);
+    merge(arr,l,mid,r);
+}
+
+void solve(){
+    ll n;
+    cin>>n;
+    ll arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    mergesort(arr,0,n);
+    for(auto x:arr)
+        cout<<x<<" ";
+    cout<<nl;
+}
+
+int main(){
+    #ifndef ONLINE_JUDGE
+        freopen("Error.txt", "w", stderr);
+    #endif
+    ll t = 1, i = 1;
+    //cin >> t;
+    cout << std::setprecision(15);
+    //presolve();
+    clock_t start, end;
+    start = clock();
+    while (t--)
+    {
+        //cout << "Case #" << i << ": ";   //only during google contest
+        //i++;                             //only during google contest
+
+        solve();
+    }
+    end = clock();
+    double Time = double(end - start) / double(CLOCKS_PER_SEC);
+    debug(Time)
+}
